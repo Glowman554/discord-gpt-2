f46b4aeaca79c3b3194a6e01afd374d0eb9fd1c1
Glowman554 <glowman434@gmail.com>
2021-11-02 21:28:46 +0100

    clear dataset of unwanted junk

--- dataset/join_datasets.js
+++ dataset/join_datasets.js
@@ -8,7 +8,12 @@ channels.forEach(function(channel) {
 	var data = Deno.readTextFileSync(`./output/${channel}.txt`);
 	var data_split = data.toString().split("\n");
 	data_split.forEach(function(line) {
-		dataset.push(line);
+		line = line.replace(/<(.*)>/g, ""); // remove pings/channel tags/nitro emotes
+		line = line.trim();
+
+		if (line != "") {
+			dataset.push(line);
+		}
 	});
 });
 
