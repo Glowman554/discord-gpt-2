3a2be0ba4d94af1b39d7ca5dea4e1a0783fb9e46
Glowman554 <glowman434@gmail.com>
2021-11-03 12:24:49 +0100

    bugfixes

--- summary.js
+++ summary.js
@@ -19,6 +19,7 @@ function generate_word_list(text) {
 		words = words.map(word => word.toLowerCase());
 		words = words.map(word => word.replace(/^(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/gm, ""));
 		words = words.map(word => word.replace(/[^a-zA-Z']/g, " "));
+		words = words.map(word => word.trim());
 
 		for (var j = 0; j < words.length; j++) {
 			if (words[j] != "") {
@@ -32,6 +33,16 @@ function generate_word_list(text) {
 				}
 			}
 		}
+	}
+
+	var num_words_used = 0;
+	for (var i = 0; i < word_list.length; i++) {
+		num_words_used += word_list[i].num_occurrences;
+	}
+
+	// calculate the percentage of the word for each word
+	for (var i = 0; i < word_list.length; i++) {
+		word_list[i].percentage = (word_list[i].num_occurrences / num_words_used) * 100;
 	}
 	
 	return word_list;
@@ -48,6 +59,7 @@ function main() {
 
 	var output = JSON.parse(Deno.readTextFileSync(prompt("Enter the name of the output file > ")));
 	var summary_object = {};
+	summary_object["raw_data"] = output;
 
 	summary_object["average_text_length"] = calculate_average_text_length(output);
 
@@ -58,17 +70,21 @@ function main() {
 
 	var words_csv = new csv();
 
-	words_csv.pushRow(["Word", "Occurrences"]);
-	words_csv.pushRow(["", ""]);
+	words_csv.pushRow(["Word", "Occurrences", "Percentage"]);
+	words_csv.pushRow(["", "", ""]);
 
 	for (var i = 0; i < word_list.length; i++) {
-		words_csv.pushRow([word_list[i].word, String(word_list[i].num_occurrences)]);
+		words_csv.pushRow([word_list[i].word, String(word_list[i].num_occurrences), String(word_list[i].percentage) + "%"]);
 	}
 
 	Deno.writeTextFileSync("output/words.csv", words_csv.serialize());
 	console.log("CSV file written to output/words.csv");
 
-	Deno.writeTextFileSync("output/words-excel.csv", words_csv.serialize(";"));
+	var excel_csv_copy = new csv();
+	excel_csv_copy.parsed_document = Object.assign([], words_csv.parsed_document);
+	excel_csv_copy.parsed_document= excel_csv_copy.parsed_document.map(row => row.map(cell => cell.indexOf("%") != 1 ? cell.replace(/\./g, ",") : cell));
+
+	Deno.writeTextFileSync("output/words-excel.csv", excel_csv_copy.serialize(";"));
 	console.log("CSV file written to output/words-excel.csv");
 
 	Deno.writeTextFileSync("output/words.txt", words_csv.str());
@@ -82,7 +98,7 @@ function main() {
 
 	console.log("Top 20 most used words: ");
 	for (var i = 0; i < 20; i++) {
-		console.log(word_list[i].word + " (" + word_list[i].num_occurrences + ")");
+		console.log(word_list[i].word + " (" + word_list[i].num_occurrences + ", " + word_list[i].percentage + "%)");
 	}
 }
 
