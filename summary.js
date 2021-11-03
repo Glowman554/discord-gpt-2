d106eaabe7bb21a0cb2f6f85ee8027c351f422a2
Glowman554 <glowman434@gmail.com>
2021-11-03 13:08:52 +0100

    bugfixes

--- summary.js
+++ summary.js
@@ -15,11 +15,14 @@ function generate_word_list(text) {
 	
 	for (var i = 0; i < text.length; i++) {
 		var words = text[i].split(" ");
+		console.log(words)
 
 		words = words.map(word => word.toLowerCase());
 		words = words.map(word => word.replace(/^(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/gm, ""));
 		words = words.map(word => word.replace(/[^a-zA-Z']/g, " "));
 		words = words.map(word => word.trim());
+		words = words.map(word => word.replace(/  /g, ""));
+		words = words.join(" ").split(" ");
 
 		for (var j = 0; j < words.length; j++) {
 			if (words[j] != "") {
